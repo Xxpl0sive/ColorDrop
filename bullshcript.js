6a5d7a57b141f206dfa17ed7d89fa606c7cf9f98
FireRat <27763388+FireRat666@users.noreply.github.com>
2024-11-04 21:19:28 +1100

    Update bullshcript.js

--- bullshcript.js
+++ bullshcript.js
@@ -43,7 +43,7 @@ async function zephiidrop() {
       await box.AddComponent(new BS.BanterColliderEvents());
       const transform = await box.AddComponent(new BS.Transform());
       transform.localScale = scale; transform.localPosition = pos;
-      box.On("trigger-enter", (e) => { if (e.detail.user !== undefined) { callback(); }});
+      box.On("trigger-enter", (e) => { if (console.log(e.detail);console.log(e.detail.user);e.detail.user !== undefined) { callback(); }});
   }   
 
   let startTime = -1, duration = 0, isStarted = false;
